be523b749fdeed1064a03f597f6c43d3dba2cfa9
Thauan <thauansilva243@gmail.com>
2026-04-30 13:33:59 -0400

    Add keymaps to split workspace

--- lua/thauan/core/keymaps.lua
+++ lua/thauan/core/keymaps.lua
@@ -7,6 +7,10 @@ keymap.set("n", "<C-j>", "<C-w>j")
 keymap.set("n", "<C-k>", "<C-w>k")
 keymap.set("n", "<C-l>", "<C-w>l")
 
+keymap.set("n", "<leader>sh", "<cmd>split<CR>")
+keymap.set("n", "<leader>sv", "<cmd>vsplit<CR>")
+keymap.set("n", "<leader>q", "<cmd>close<CR>")
+
 keymap.set("n", "<leader>t1", "<cmd>ToggleTerm 1<CR>")
 keymap.set("n", "<leader>t2", "<cmd>ToggleTerm 2<CR>")
 keymap.set("n", "<leader>t3", "<cmd>ToggleTerm 3<CR>")
